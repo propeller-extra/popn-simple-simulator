86d1101dc6bd886f657bcadc76db8a6e99639f59
propeller-extra <node.js@zoho.com>
2020-05-07 20:11:10 +0900

    gulpでejs->htmlを出力

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var fs = require('fs');
+var ejs = require('gulp-ejs');
+var rename = require('gulp-rename');
+
+//EJS(テンプレートエンジン)
+gulp.task("ejs", function (done) {
+    gulp.src(
+        ["views/*.ejs"] // "app.js"  と  "views/*.ejs"  を手動で切り替えろ！！複数指定のやり方とか知らんｗｗｗ
+    )
+        .pipe(ejs())
+        .pipe(rename({
+            extname: ".html"
+        })) //拡張子をhtmlに
+        .pipe(gulp.dest("dest/")); //出力先
+    done();
+});
